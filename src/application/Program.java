0e28e8461a5b3dbac273d326266ca81af04f4faa
André Aragão <64162795+AndreAragaoSoftware@users.noreply.github.com>
2020-06-03 11:41:26 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,16 +28,23 @@ public class Program {
 		}
 		System.out.println();
 	
-		System.out.println("=== TEST 3: seller findAll===");
+		System.out.println("=== TEST 3: seller findAll ===");
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 	    	}
 		System.out.println();
 		
-		System.out.println("=== TEST 4: seller insert===");
+		System.out.println("=== TEST 4: seller insert ===");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		System.out.println();
+		
+		System.out.println("=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 }
